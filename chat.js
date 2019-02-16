0528a1f01599d81c3a667058f32c00b914e3255b
maxhat <maksmak200305@gmail.com>
2019-02-16 18:09:15 +0300

    //Update 9.99// Цвет меняется

--- chat.js
+++ chat.js
@@ -133,5 +133,5 @@ function userCreate(user, uid) {
 	var red = colors[0];
 	var green = colors[1];
 	var blue = colors[2];
-	document.getElementById(uid).style.backgroundColor ="rgb(red,green,blue)";
+	document.getElementById(uid).style.backgroundColor = "rgb(" + red + "," + green + "," + blue +")";
 }
